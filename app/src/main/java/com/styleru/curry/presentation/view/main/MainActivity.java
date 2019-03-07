98d8c5a7692358dd8131cb7aaeb692e65696f3e8
Vitaly <kvitaly21@yandex.ru>
2019-03-07 23:27:34 +0300

    Initial commit

---
+++ app/src/main/java/com/styleru/curry/presentation/view/main/MainActivity.java
@@ -0,0 +1,32 @@
+package com.styleru.curry.presentation.view.main;
+
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.styleru.curry.R;
+
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+        init();
+    }
+
+    private void initViews(){
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+    }
+
+    private void init(){
+        NavigationUI.setupWithNavController(bottomNavigationView, Navigation.findNavController(this, R.id.nav_host_fragment));
+    }
+}
---
+++ app/src/main/java/com/styleru/curry/presentation/view/recipe/RecipeFragment.java
@@ -0,0 +1,21 @@
+package com.styleru.curry.presentation.view.recipe;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.styleru.curry.R;
+
+public class RecipeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recipe, container, false);
+    }
+
+}
